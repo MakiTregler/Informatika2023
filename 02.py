05522c64ce995e92b7b7f89f752f0e591081a7bf
Markéta Treglerová <144822035+MakiTregler@users.noreply.github.com>
2023-09-20 12:00:54 +0000

    03.py

---
+++ 02.py
@@ -0,0 +1,25 @@
+print(2 * 2 * 2)
+print
+print(2 ** 10)
+
+for promenna in [1, 2, 3, 4, 5]:
+    print (promenna)
+
+for promenna in range(5):
+    print(promenna)
+
+zaklad = 2
+
+for x in range(10):
+    print(x ** 2)
+
+print(zaklad)
+
+soucet = 0
+
+for cislo in range(1, 101):
+    soucet = soucet + cislo
+    prvni = cislo
+    druhe = 101 - cislo
+
+print(soucet)
