a7338ab6ddb2c309ac540eef9557a6aecd6cacaf
Markéta Treglerová <144822035+MakiTregler@users.noreply.github.com>
2023-11-01 12:24:49 +0000

    kvadr

---
+++ 05.py
@@ -0,0 +1,15 @@
+a = float(input("a="))
+b = float(input("b="))
+c = float(input("c="))
+
+def solve_quadratic_equation(a, b, c):
+    print(f"{a}*x*x + {b}*x + {c}")
+
+    d = b**2 - 4*a*c
+    x1 = (-b + d**0.5)/2*a
+    x2 = (-b - d**0.5)/2*a
+    result = f"K= {x1}, {x2}"
+
+    return result
+
+print(solve_quadratic_equation(a, b, c))
