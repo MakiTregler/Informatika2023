97bf0e5e481c527a3b94b6c3de97b31e03fbb2b2
Markéta Treglerová <144822035+MakiTregler@users.noreply.github.com>
2023-11-01 12:38:46 +0000

    kvadre

--- quadraticequation.py
+++ quadraticequation.py
@@ -6,8 +6,8 @@ def solve_quadratic_equation(a, b, c):
     print(f"{a}*x*x + {b}*x + {c}")
 
     d = b**2 - 4*a*c
-    x1 = (-b + d**0.5)/2*a
-    x2 = (-b - d**0.5)/2*a
+    x1 = (-b + d**0.5)/(2*a)
+    x2 = (-b - d**0.5)/(2*a)
     result = f"K= {x1}, {x2}"
 
     return result
